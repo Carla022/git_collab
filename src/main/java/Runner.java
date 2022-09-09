3400efad4b06ea482a9d06003ef0e72f1ba9e689
Carla022 <cs.morenol@outlook.com>
2022-09-09 11:55:43 +0100

    adds sout about develop

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,9 @@
+import java.sql.SQLOutput;
+
 public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("We're working on develop!");
     }
 }

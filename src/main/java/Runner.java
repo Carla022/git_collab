af4e58e0eacb8fdf6e62cae6c33c6ff079f69e07
Carla022 <cs.morenol@outlook.com>
2022-09-09 12:14:35 +0100

    pull request process 1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,8 @@
-import java.sql.SQLOutput;
-
 public class Runner {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("We're working on develop!");
+        System.out.println("We're making a pull request");
     }
 }
